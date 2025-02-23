c2ccac270a1e60dd286a12816d9496cb382b12d4
RuiJ72 <rlagos900@gmail.com>
2025-02-23 19:35:32 -0300

    Variables and Github

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,13 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Homework - Working with Variables and GitHub");
+            Console.Write("\nPlease enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Now, enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nYour name is {name} and you have {age} old!");
         }
     }
 }
